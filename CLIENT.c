265ec50e7115c846fdbb11ba3dac2fa09492ffb5
AnnaMD01 <ana94maria94@gmail.com>
2017-07-25 09:19:40 +0300

    Add files via upload

--- CLIENT.c
+++ CLIENT.c
@@ -4,33 +4,60 @@
 #include <string.h>
 #include <mqueue.h>	//for messages queues
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
-#define MESSAGE_QUEUE_CLIENT_NAME "client"
-#define MESSAGE_QUEUE_SERVER_NAME "server"
+//#define QUEUE_CLIENT_NAME "/client"
+#define QUEUE_SERVER_NAME "/server"
 #define SIZE 256
+#define MG_MAXMSG 10
+#define MG_MSGSIZE 200
 
 int main()
 {
 	mqd_t client, server;
-	char message_from_client[56];
-	sprintf (message_from_client, "process id is %d", getpid ());
 	char message_from_server[SIZE];
+	char queue_client_name[56];
+	sprintf(queue_client_name,"/client-%d", getpid());
+	//sprintf("%s\n", queue_client_name);
+	
+	struct mq_attr attr;
 
-	if((client = mq_open(MESSAGE_QUEUE_CLIENT_NAME, O_CREAT	| O_RDWR, 0666, NULL)) != -1)
-	{	
-		printf("Just for testing!\n");
-		server = mq_open(MESSAGE_QUEUE_SERVER_NAME, O_WRONLY);
-		mq_send(server, message_from_client, strlen(message_from_client), 0);
-		mq_receive(client, message_from_server, SIZE, NULL);
-
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = MG_MAXMSG;
+    attr.mq_msgsize = MG_MSGSIZE;
+    attr.mq_curmsgs = 0;
+	
+	if((client = mq_open(queue_client_name, O_CREAT	| O_RDWR, 0666, &attr)) == -1)
+	{
+		printf("Client can create /open a messages queue\n");
+		exit(0);
+	}
+		
+	if((server = mq_open(QUEUE_SERVER_NAME, O_WRONLY)) == -1)
+	{
+		printf("Client cann't access the server's messages queue\n");
+		exit(0);
+	}
+		
+	if((mq_send(server, queue_client_name, strlen(queue_client_name), 0)) == -1)
+	{
+		printf("Client cann't send a messages to the server\n");
+		exit(0);
+	}
+		
+	if((mq_receive(client, message_from_server, SIZE, NULL)) == -1)
+	{
+		printf("Client cann't receive a message from the server\n");
+		exit(0);
+	}
+		
 		printf("The token number that have just been received from the server is: %s\n",message_from_server);
 
 		//close and erase the message queue
 		mq_close(client);
-		mq_unlink(message_from_client);
-	}
+		mq_unlink(queue_client_name);
+	
 
-
-	return 0;
+	 return 0;
 }
--- SERVER.c
+++ SERVER.c
@@ -3,33 +3,65 @@
 #include <string.h>
 #include <mqueue.h>	//for messages queues
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
-#define MESSAGE_QUEUE_SERVER_NAME	"server"
+#define QUEUE_SERVER_NAME	"/server"
 #define SIZE 	256
+#define MG_MAXMSG 10
+#define MG_MSGSIZE 200
+
 
 int main()
 {
 	mqd_t server,client;	//messages queue identifier
-	long tokenNumber = 0;
-	char buffer_receive_server[SIZE];
-	char buffer_send_to_client[SIZE];
+	long int tokenNumber = 0;
+	char receive_from_client[SIZE];
+	char send_to_client[SIZE];
 
-	
-	
-	if((server = mq_open(MESSAGE_QUEUE_SERVER_NAME,	O_CREAT	| O_RDWR, 0666, NULL)) != -1);
-		{
-			while(1) // a server should always listen
-			{	
-				mq_receive(server, buffer_receive_server, SIZE, NULL);
-				client = mq_open(buffer_receive_server, O_WRONLY);
-				sprintf(buffer_send_to_client,"%ld",tokenNumber);
-				mq_send(client, buffer_send_to_client, strlen(buffer_send_to_client), 0);
-				//mq_close(client);
-				tokenNumber++;
-			}
+	struct mq_attr attr;
+
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = MG_MAXMSG;
+    attr.mq_msgsize = MG_MSGSIZE;
+    attr.mq_curmsgs = 0;
+
+	printf("Server is waiting for messages...\n");
+
+	if((server = mq_open(QUEUE_SERVER_NAME,	O_CREAT	| O_RDWR, 0666, &attr)) == -1)
+	{	
+		printf("Server is not able to create / open a messages queue!\n");
+		exit(0);
+	}
+			
+	while(1) // a server should always listen
+	{	
+				
+		//memset(receive_from_client,0,SIZE);
+		//memset(send_to_client,0,SIZE);
+
+		if((mq_receive(server, receive_from_client, SIZE, NULL)) == -1)
+		{	perror("Server receive");
+			printf("Server can receive messages!\n");
+			exit(0);
 		}
-	
+
+		if((client = mq_open(receive_from_client, O_WRONLY)) == -1)
+		{
+			printf("The server can access the client message queue\n");
+			exit(0);
+		}
+
+		sprintf(send_to_client,"%ld",tokenNumber);
+		if((mq_send(client, send_to_client, strlen(send_to_client), 0)) == -1)
+		{
+			printf("Sorry...the server can send a response to the client\n");
+			exit(0);
+		}
+
+		tokenNumber++;
+	}
+		
 
 	return 0;
 
